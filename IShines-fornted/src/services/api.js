c20ed4b5c92664ca884448bca3949c5346315fed
Taha Junaid <studenttahaofficial@gmail.com>
2026-04-09 00:47:22 +0500

    Delete IShines-fornted directory

--- IShines-fornted/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// API service placeholder
-export const fetchProducts = async () => {
-  // implement fetch logic
-};
-
-export const placeOrder = async order => {
-  // implement order placement logic
-};
